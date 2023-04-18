c13deacf1f07c2c2cc15faabeaca38662752e79e
emily <biteloemily@gmail.com>
2023-04-18 20:35:36 -0300

    exercitando strings e arrays

---
+++ script.js
@@ -0,0 +1,20 @@
+//EXERCICIOS ARRAY
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)//11
+
+let i = 0
+console.log('d. ', array[i])//3
+
+array[i+1] = 19
+console.log('e. ', array)//1 = 19
+
+const valor = array[i+6]
+console.log('f. ', valor)//f = 9
+
