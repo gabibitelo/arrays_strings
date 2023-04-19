e11d547c4ec590f8fe42cfb1ecb294f606f653cf
emily <biteloemily@gmail.com>
2023-04-18 22:00:13 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 //EXERCICIOS ARRAY
-let array
-console.log('a. ', array)
+//let array
+//console.log('a. ', array)
 
-array = null
-console.log('b. ', array)
+//array = null
+//console.log('b. ', array)
 
 
-array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-console.log('c. ', array.length)//11
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)//11
 
-let i = 0
-console.log('d. ', array[i])//3
+//let i = 0
+//console.log('d. ', array[i])//3
 
-array[i+1] = 19
-console.log('e. ', array)//1 = 19
+//array[i+1] = 19
+//console.log('e. ', array)//1 = 19
 
-const valor = array[i+6]
-console.log('f. ', valor)//f = 9
+//const valor = array[i+6]
+//console.log('f. ', valor)//f = 9
+
+//const frase = prompt("digite uma frase")
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+//vai ter privi 13
+
+
+//1. fça um programa que pergunte ao usario seu nome e seu
+//e mail.em seguida imprima no console a seguinte mensagem 
+
+//o e mail foi cadastrado com suceso.
+//seja bem vindo, `nomeDoUsario`!
+
+
+//const frase = prompt("qual seu nome? ")
+//const frase2 = prompt("qual seu e-mail ")
+//alert("o e-mail" +frase2+" foi cadastrado com sucesso")
+//alert("seja bem vinda/o "+ frase);
+
+//3. fça um programa seguindo os passos:
+//A)crie um array vazio e guarde-o em uma variavel, chamada lista de tar
+//refas
+
+let array;
+let listadetarefas = array;
+
+
+//B)pergunte ao usario 3 tarefas que ele precisa realizar no dia e arma
+//zene-as uma por uma no array
+
+const listaDeTarefas = ("lavar-a-loça", "dobar-a-roupa", "pasar-pano")
+
+//C)imprima o array no console
+//D)pça ao usario que digite o **indice** de uma tarefa que ele já 
+//realizou: 0, 1 ou 2
+//E)remova da lista o item de indice que o usuario escolheu
+//F)imprima o array no console 
 
